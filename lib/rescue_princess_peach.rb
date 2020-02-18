1554a891aa6d15a86fe256832ea8e6f7db7583c4
heyserenadinh Dinh <heyserenadinh@gmail.com>
2020-02-18 00:01:01 +0000

    Done.

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,9 +1,8 @@
-def rescue_princess_peach
-  status = "rescued"
-  puts "Hooray! Mario has rescued Princess Peach."
+require "spec_helper"
+ 
+describe "#rescue_princess_peach" do
+  it "outputs a message and sets a variable, status, that is not available outside of this method" do
+    expect{rescue_princess_peach}.to output("Hooray! Mario has rescued Princess Peach.\n").to_stdout
+    expect{puts status}.to raise_error(NameError)
+  end
 end
-
-## Comment the below back in to see method scope in action!
-
-# rescue_princess_peach
-# puts status
